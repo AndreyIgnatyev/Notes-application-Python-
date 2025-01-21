d095505bdc23d3719b954dd2c9f45e8232e5178c
Andrey <booogerman12345@gmail.com>
2025-01-22 00:01:07 +0500

    Добавил функцию фильтра по дате

--- notes.py
+++ notes.py
@@ -47,7 +47,7 @@ def read_notes():
     for note in notes:
         print(f"ID: {note['id']}, Заголовок: {note['title']}, Дата: {note['timestamp']}")
         print(f"Тело: {note['body']}\n")
-        
+
 # Функция чтения одной заметки по ID
 def read_note_by_id():
     notes = load_notes()
@@ -65,3 +65,32 @@ def read_note_by_id():
             print(f"Тело: {note['body']}")
             return
     print("Заметка с таким ID не найдена.")
+
+# Функция выборки заметок по дате
+def filter_notes_by_date():
+    notes = load_notes()
+    if not notes:
+        print("Заметок нет.")
+        return
+
+    date_input = input("Введите дату для фильтрации (формат: ГГГГ-ММ-ДД): ")
+    try:
+        filter_date = datetime.strptime(date_input, "%Y-%m-%d").date()
+    except ValueError:
+        print("Ошибка: Неверный формат даты. Используйте ГГГГ-ММ-ДД.")
+        return
+
+    found_notes = []
+    for note in notes:
+        note_date = datetime.strptime(note['timestamp'], "%Y-%m-%d %H:%M:%S").date()
+        if note_date == filter_date:
+            found_notes.append(note)
+
+    if found_notes:
+        print(f"Найдены заметки за дату {date_input}:")
+        for note in found_notes:
+            print(f"ID: {note['id']}, Заголовок: {note['title']}, Дата: {note['timestamp']}")
+            print(f"Тело: {note['body']}\n")
+    else:
+        print(f"Заметок за дату {date_input} не найдено.")
+        
